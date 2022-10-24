7fa4cf7ee856a1afe2ccc34e4a5d61b91629d671
stakansoljarki <roman.dybach@gmail.com>
2022-10-24 21:22:03 +0300

    fix

--- src/app/layouts/Composite/Composite.jsx
+++ src/app/layouts/Composite/Composite.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import image from "./images/composite.jpg"
+
 const Composite = () => {
     return (
         <section id="home" className="composite">
             <h1 className="composite__title">Світ Композитів</h1>
+            <img src={image} alt="" className="composite__image"/>
         </section>
     )
 }
